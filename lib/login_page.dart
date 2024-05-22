d394a0f834eca3380b8f1b2f75a17f1402b4036e
mamun <mamunjoy4291@gmail.com>
2024-05-22 14:00:57 +0600

    Complete LogIn Page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -18,26 +18,99 @@ class _HomePageState extends State<HomePage> {
     leading: Icon(Icons.arrow_back_ios_new),
   ),
 
-      body:Column (
-        mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment: CrossAxisAlignment.center,
+      body:Column(
         children: [
-          Row(
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 150,
-                height: 130,
-               child:  Image.asset("assets/flutter5786.jpg"),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              child: Column (
+                mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 200,
+                       child:  Image.asset("assets/flutter5786.jpg"),
+                      ),
+
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Flutter",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w300
+                      ),),
+                    ],
+                  ),
+
+                  SizedBox(
+                    height:15,
+                  ),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                      border: OutlineInputBorder()
+                    ),
+                  ),
+
+                  SizedBox(
+                    height:25,
+                  ),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Password",
+                        border: OutlineInputBorder()
+                    ),
+                  ),
+
+                  SizedBox(
+                    height:25,
+                  ),
+
+                  Text("Forget Password?",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.lightBlue
+                      )
+                  ),
+                  SizedBox(
+                    height:25,
+                  ),
+
+                  Container(
+                    height: 50,
+                    width: 400,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text("LogIn",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                    ),
+                  ),
+
+
+                ],
               ),
-              Text("Flutter",
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.w300
-              ),),
-            ],
+            ),
           ),
+          Container(
+            child: Text("Dont have an Account? Create Account",),
+            margin: EdgeInsets.only(bottom: 20),
+          )
         ],
       ),
     );
